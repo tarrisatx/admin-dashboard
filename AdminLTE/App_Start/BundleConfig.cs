13983c6399c5fb6540a6fd9628f9d7250eef14e5
Tahara Ezell <taharamezell@gmail.com>
2019-03-22 01:37:06 -0500

    Add project files.

---
+++ AdminLTE/App_Start/BundleConfig.cs
@@ -0,0 +1,41 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace AdminLTE
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css",
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/iconicons.min.css",
+                      "~/admin-lte/css/AdminLTE.css",
+                      "~/admin-lte/css/skins/skin-blue.css",
+                      "~/admin-lte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css"));
+
+            bundles.Add(new ScriptBundle("~/admin-lte/js").Include(
+             "~/admin-lte/js/app.js",
+             "~/admin-lte/plugins/fastclick/fastclick.js",
+             "~/admin-lte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"));
+        }
+    }
+}
